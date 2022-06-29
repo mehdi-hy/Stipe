4666c2739b503a368d8e4f6ebbb7e999f89e4b31
mehdi-hy <85339536+mehdi-hy@users.noreply.github.com>
2022-06-29 13:46:53 -0700

    Add files via upload

---
+++ build/precache-manifest.96939fb03366313a6a871e186b5e9d65.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6d9cc884dea110085c50d5d31a3f4b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35d3ad6d46ac84d42577",
+    "url": "/static/css/main.d4ca8e90.chunk.css"
+  },
+  {
+    "revision": "9d9ce4078b442a71cc60",
+    "url": "/static/js/2.e1e77988.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e1e77988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35d3ad6d46ac84d42577",
+    "url": "/static/js/main.cfab02f4.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  },
+  {
+    "revision": "8596ab4a635cf6b6ea535b15bdc0dc67",
+    "url": "/static/media/hero.8596ab4a.svg"
+  },
+  {
+    "revision": "1090473dd728fcc17033df70118fa668",
+    "url": "/static/media/logo.1090473d.svg"
+  },
+  {
+    "revision": "58d7e3d689c0cb8eb365b86046ecdce8",
+    "url": "/static/media/phone.58d7e3d6.svg"
+  }
+]);
